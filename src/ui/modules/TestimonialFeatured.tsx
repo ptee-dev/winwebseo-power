42a3327861dd5364b8fa391c90cc63682606b4b8
Cherdkiat Taesookavat <cherdkiat@gmail.com>
2025-07-02 16:57:09 +0700

    edit css TeatimonialFeature

--- src/ui/modules/TestimonialFeatured.tsx
+++ src/ui/modules/TestimonialFeatured.tsx
@@ -12,48 +12,44 @@ export default function TestimonialFeatured({
 	const { author } = testimonial
 
 	return (
-		<section className="section">
-			<div className="section bg-accent/3 flex max-w-screen-md items-center gap-x-12 gap-y-6 rounded max-sm:flex-col">
-				<div className="space-y-2">
-					<VscQuote className="text-accent inline-block shrink-0 text-4xl" />
+<section className="section">
+  <div className="bg-accent/5 flex max-w-screen-md mx-auto p-6 items-center gap-x-12 gap-y-6 rounded max-sm:flex-col">
+    <div className="space-y-2">
+      <VscQuote className="text-accent inline-block shrink-0 text-4xl" />
+      <div className="self-center text-xl text-balance">
+        <PortableText value={testimonial.content} />
+      </div>
+      <dl className="text-start">
+        <dt className="flex flex-wrap items-center gap-1">
+          {author?.name}
+          {testimonial?.source && (
+            <cite>
+              <a
+                className="text-ink/50"
+                href={testimonial.source}
+                target="_blank"
+                title="Source"
+              >
+                <VscSurroundWith />
+              </a>
+            </cite>
+          )}
+        </dt>
+        {author?.title && (
+          <dd className="text-sm text-balance">{author?.title}</dd>
+        )}
+      </dl>
+    </div>
 
-					<div className="self-center text-xl text-balance">
-						<PortableText value={testimonial.content} />
-					</div>
-
-					<dl className="text-start">
-						<dt className="flex flex-wrap items-center gap-1">
-							{author?.name}
-
-							{testimonial?.source && (
-								<cite>
-									<a
-										className="text-ink/50"
-										href={testimonial.source}
-										target="_blank"
-										title="Source"
-									>
-										<VscSurroundWith />
-									</a>
-								</cite>
-							)}
-						</dt>
-
-						{author?.title && (
-							<dd className="text-sm text-balance">{author?.title}</dd>
-						)}
-					</dl>
-				</div>
-
-				<Img
-					className="mx-auto max-w-[200px] shrink-0 rounded"
-					image={author?.image}
-					width={400}
-					alt={
-						[author?.name, author?.title].filter(Boolean).join(', ') || 'Author'
-					}
-				/>
-			</div>
-		</section>
+    <Img
+      className="mx-auto max-w-[200px] shrink-0 rounded"
+      image={author?.image}
+      width={400}
+      alt={
+        [author?.name, author?.title].filter(Boolean).join(', ') || 'Author'
+      }
+    />
+  </div>
+</section>
 	)
 }
